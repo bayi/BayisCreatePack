e478cbff1b3a268913dd22177963e1a5391482d0
Bayi <bayi@bayi.hu>
2025-08-24 17:15:01 +0200

    Textures

--- src/startup_scripts/leaf_mulcher.js
+++ src/startup_scripts/leaf_mulcher.js
@@ -5,9 +5,14 @@ StartupEvents.registry('block', event => {
     .soundType('wood')
     .hardness(1.0)
     .resistance(1.0)
+    .fullBlock(false)
+    .defaultCutout()
+    .box(2, 0, 2, 14, 12, 14) // Make it slightly smaller than a full block
+    .texture('bottom', 'minecraft:block/oak_planks')
+    .texture('side', 'minecraft:block/composter_side')
+    .texture('top', 'createskyblock:block/example_block')
     .tagBlock('createskyblock:leaf_mulchers')
     .tagBlock('minecraft:mineable/axe')
-    .texture('createskyblock:block/example_block')
 })
 
 StartupEvents.modifyCreativeTab('minecraft:functional_blocks', event => {
