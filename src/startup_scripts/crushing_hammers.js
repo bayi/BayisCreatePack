b4a9ad1799f7d0afd8a38809dfa72a8d7a220a94
Bayi <bayi@bayi.hu>
2025-08-24 14:32:05 +0200

    Fix hammer durability

--- src/startup_scripts/crushing_hammers.js
+++ src/startup_scripts/crushing_hammers.js
@@ -17,9 +17,17 @@ StartupEvents.registry('item', event => {
   crushingHammers.items.push(['createskyblock:crushing_hammer', 'minecraft:oak_planks'])
   event.create('createskyblock:crushing_hammer', 'pickaxe')
     .maxStackSize(1)
-    .maxDamage(250) // durability
     .texture('createskyblock:item/crushing_hammer')
-    .tier('stone')
+    .tier(event => {
+      return {
+        level: 1, // stone level
+        uses: 250,
+        speed: 4.0,
+        attackDamageBonus: 1.0,
+        enchantmentValue: 5,
+        tag: crushingHammers.tag
+      }
+    })
     .tag(crushingHammers.tag) // custom tag for all crushing hammers
     .tag('minecraft:mineable/pickaxe') // behaves like a pickaxe for mining
     .tag('forge:tools')
