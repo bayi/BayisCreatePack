df8e9994d0dd5e89d3997e7118a288c4d9197c99
Bayi <bayi@bayi.hu>
2025-09-06 17:05:04 +0200

    Dummy tags

---
+++ src/server_scripts/tags.js
@@ -0,0 +1,19 @@
+ServerEvents.tags('items', (event) => {
+
+  // event.add('c:seeds', 'supplementaries:flax_seeds')
+
+  /*
+  event.add('createskyblock:seeds', [
+    // Basic Minecraft Seeds
+    'minecraft:wheat_seeds', 'minecraft:melon_seeds', 'minecraft:pumpkin_seeds', 'minecraft:beetroot_seeds',
+    // Farmer's Delight Seeds
+    'farmersdelight:cabbage_seeds', 'farmersdelight:tomato_seeds',
+
+    // Supplementaries
+    'supplementaries:flax_seeds',
+
+  ])
+  */
+
+
+})
