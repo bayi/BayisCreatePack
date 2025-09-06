9d1de3211fcb07a5ac50fe0f8ff2949a62f46980
Bayi <bayi@bayi.hu>
2025-09-06 10:10:17 +0200

    Seed strings

---
+++ src/client_scripts/seeds.js
@@ -0,0 +1,13 @@
+ClientEvents.lang('en_us', (event) => {
+  event.add('item.createskyblock.grass_seeds', 'Grass Seeds')
+  event.add('item.createskyblock.mycelium_seeds', 'Mycelium Seeds')
+  event.add('item.createskyblock.warped_nylium_seeds', 'Warped Nylium Seeds')
+  event.add('item.createskyblock.crimson_nylium_seeds', 'Crimson Nylium Seeds')
+})
+
+ClientEvents.lang('hu_hu', (event) => {
+  event.add('item.createskyblock.grass_seeds', 'Fűmag')
+  event.add('item.createskyblock.mycelium_seeds', 'Micélium mag')
+  event.add('item.createskyblock.warped_nylium_seeds', 'Torz nílium mag')
+  event.add('item.createskyblock.crimson_nylium_seeds', 'Karmazsin nílium mag')
+})
